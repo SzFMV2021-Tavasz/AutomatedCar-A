298279f32f2dd29db3fa7980247528eff7e1b7b2
Daniel Meszaros <easimer@gmail.com>
2021-02-28 14:19:20 +0100

    Implement drag force; fix argument mix-up
    - Implement drag force calculation
      - Remove argument `wheelRotation` (added accidentally, this force
        doesn't use it

--- src/AutomatedCar/SystemComponents/Powertrain/VehicleForces.cs
+++ src/AutomatedCar/SystemComponents/Powertrain/VehicleForces.cs
@@ -5,7 +5,8 @@ namespace AutomatedCar.SystemComponents.Powertrain
 {
     public class VehicleForces
     {
-        private IVehicleConstants vehicleConstants;
+        private readonly IVehicleConstants vehicleConstants;
+        private readonly float epsilon = 0.001f;
 
         public VehicleForces(IVehicleConstants vehicleConstants)
         {
@@ -15,12 +16,24 @@ namespace AutomatedCar.SystemComponents.Powertrain
         /// <summary>
         /// Calculates the drag force acting on a particle.
         /// </summary>
-        /// <param name="wheelRotation">Rotation of the wheel in the counter-clockwise direction, in radians.</param>
-        /// <param name="v">Speed of the particle.</param>
+        /// <param name="velocity">Speed of the particle.</param>
         /// <returns>Drag force.</returns>
-        public Vector2 GetDragForce(float wheelRotation, Vector2 v)
+        public Vector2 GetDragForce(Vector2 velocity)
         {
-            throw new NotImplementedException();
+            var relativeVelocity = Vector2.Zero - velocity;
+
+            // If the relative velocity is (near) zero we pretend that there is
+            // no drag force present, since a zero vector cannot be normalized.
+            if (relativeVelocity.LengthSquared() < epsilon)
+            {
+                return Vector2.Zero;
+            }
+
+            var forceDir = Vector2.Normalize(relativeVelocity);
+            var velocitySquared = relativeVelocity.LengthSquared();
+            var dragArea = vehicleConstants.DragCoefficient * vehicleConstants.CrossSectionalArea;
+
+            return 0.5f * vehicleConstants.AirDensity * velocitySquared * dragArea * forceDir;
         }
     }
 }
--- test/AutomatedCarTest/SystemComponents/Powertrain/VehicleForcesTests.cs
+++ test/AutomatedCarTest/SystemComponents/Powertrain/VehicleForcesTests.cs
@@ -17,10 +17,9 @@ namespace AutomatedCarTest.SystemComponents.Powertrain
             var forceCalculator = new VehicleForces(constants.Object);
             var vel0 = Vector2.Zero;
             var vel1 = new Vector2(0, 28);
-            var wheelRotation = 0f;
 
-            var force0 = forceCalculator.GetDragForce(wheelRotation, vel0);
-            var force1 = forceCalculator.GetDragForce(wheelRotation, vel1);
+            var force0 = forceCalculator.GetDragForce(vel0);
+            var force1 = forceCalculator.GetDragForce(vel1);
 
             Assert.True(force0.Length() < force1.Length());
         }

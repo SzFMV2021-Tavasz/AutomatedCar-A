a8413c44c2a7f3e9d7506041c8bb109062722b02
Kuklin István Alexander <istvan@codekuklin.com>
2021-03-04 19:33:29 +0100

    Refactor

--- BaseModel/JSONPolygonDictionaryDeserializer.cs
+++ BaseModel/JSONPolygonDictionaryDeserializer.cs
@@ -34,35 +34,67 @@ namespace BaseModel
         /// </summary>
         public Dictionary<WorldObject.Type, List<Polygon>> Load(string polygonsPath)
         {
-            Dictionary<WorldObject.Type, List<Polygon>> polygonDictionary = new Dictionary<WorldObject.Type, List<Polygon>>();
+            return ParseJson(File.ReadAllText(polygonsPath));
+        }
 
-            string jsonFileContent = File.ReadAllText(polygonsPath);
+        private Dictionary<WorldObject.Type, List<Polygon>> ParseJson(string jsonFileContent)
+        {
+            try
+            {
+                JObject j_full = JsonConvert.DeserializeObject<JObject>(jsonFileContent);
+                return ParseJson_Objects(j_full["objects"]);
+            }
+            catch (NullReferenceException e)
+            {
+                throw new JSONReadingException("Failed to parse JSON.", e);
+            }
+            
+        }
 
-            JObject j_full = JsonConvert.DeserializeObject<JObject>(jsonFileContent);
-            var j_objects = j_full["objects"];
-
+        private Dictionary<WorldObject.Type, List<Polygon>> ParseJson_Objects(JToken j_objects)
+        {
+            Dictionary<WorldObject.Type, List<Polygon>> polygonDictionary = new();
+            
             foreach (var j_object in j_objects)
             {
-                WorldObject.Type type = this.typenamesStringDictionaryReverse[j_object["typename"].ToString()];
-                List<Polygon> polys = new();
-
-                foreach (var j_poly in j_object["polys"])
-                {
-                    Polygon.Type_t polyType = PolygonTypeDict[j_poly["type"].ToString()];
-                    List<Tuple<int, int>> points = new();
-
-                    foreach (var j_point in j_poly["points"])
-                    {
-                        points.Add(new Tuple<int, int>(int.Parse(j_point[0].ToString()), int.Parse(j_point[1].ToString())));
-                    }
-
-                    polys.Add(new Polygon(polyType, points.ToArray()));
-                }
-                
-                polygonDictionary.Add(type, polys);
+                ParseJson_Objects_IntoDict(j_object, polygonDictionary);
             }
 
             return polygonDictionary;
         }
+
+        private void ParseJson_Objects_IntoDict(JToken j_object, Dictionary<WorldObject.Type, List<Polygon>> polygonDictionary)
+        {
+            WorldObject.Type type = this.typenamesStringDictionaryReverse[j_object["typename"].ToString()];
+            
+            List<Polygon> polys = new();
+
+            foreach (var j_poly in j_object["polys"])
+            {
+                Polygon.Type_t polyType = PolygonTypeDict[j_poly["type"].ToString()];
+                List<Tuple<int, int>> points = ParseJson_Points(j_poly);
+                polys.Add(new Polygon(polyType, points.ToArray()));
+            }
+
+            polygonDictionary.Add(type, polys);
+        }
+
+        private static List<Tuple<int, int>> ParseJson_Points(JToken j_poly)
+        {
+            List<Tuple<int, int>> points = new();
+        
+            foreach (var j_point in j_poly["points"])
+            {
+                points.Add(ParseJson_Point(j_point));
+            }
+            
+            return points;
+        }
+
+        private static Tuple<int, int> ParseJson_Point(JToken j_point)
+        {
+            return new(int.Parse(j_point[0].ToString()), int.Parse(j_point[1].ToString()));
+        }
+
     }
 }

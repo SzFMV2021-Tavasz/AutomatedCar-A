8f6e4d23199b5827b2d79482137f3b6189bfd1c8
Szabolcs Bakos <bakosszaben@gmail.com>
2021-03-04 20:58:43 +0100

    Added PowertrainAutomaticallyGetsRegisteredToVFB test.

---
+++ test/AutomatedCarTest/SystemComponents/Powertrain/PowertrainComponentTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedCar.SystemComponents.Powertrain;
+using AutomatedCar.SystemComponents;
+using Moq;
+using Xunit;
+
+namespace Test.SystemComponents.Powertrain
+{
+    public class PowertrainComponentTests
+    {
+        [Fact]
+        public  void PowertrainAutomaticallyGetsRegisteredToVFB()
+        {
+            Mock<IVirtualFunctionBus> mockBus = new Mock<IVirtualFunctionBus>();
+            PowertrainComponent ptr = new PowertrainComponent(mockBus.Object);
+
+            mockBus.Verify(m => m.RegisterComponent(ptr), Times.Once);
+        }
+    }
+}

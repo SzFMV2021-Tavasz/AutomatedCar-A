e9eb7e7452d193f54de07fee891815afe39eaa58
Szabolcs Bakos <bakosszaben@gmail.com>
2021-03-04 15:45:58 +0100

    Added Powertrain class

---
+++ src/AutomatedCar/SystemComponents/Powertrain/Powertrain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCar.SystemComponents.Powertrain
+{
+    public class Powertrain : SystemComponent
+    {
+        public Powertrain(VirtualFunctionBus virtualFunctionBus)
+           : base(virtualFunctionBus)
+        {
+            
+        }
+        public override void Process()
+        {
+            
+        }
+    }
+}

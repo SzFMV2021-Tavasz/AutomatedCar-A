0fa50a0bbd039a36388fb795d55776db23ec80dd
Szabolcs Bakos <bakosszaben@gmail.com>
2021-03-04 21:03:29 +0100

    Powertrain now creates new PowertrainPacket.

--- src/AutomatedCar/SystemComponents/Powertrain/PowertrainComponent.cs
+++ src/AutomatedCar/SystemComponents/Powertrain/PowertrainComponent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutomatedCar.SystemComponents.Packets;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,12 @@ namespace AutomatedCar.SystemComponents.Powertrain
 {
     public class PowertrainComponent : SystemComponent
     {
+        private PowertrainComponentPacket powertrainPacket;
         public PowertrainComponent(IVirtualFunctionBus virtualFunctionBus)
            : base(virtualFunctionBus)
         {
-            
+            this.powertrainPacket = new PowertrainComponentPacket();
+            virtualFunctionBus.PowertrainPacket = this.powertrainPacket;
         }
         public override void Process()
         {

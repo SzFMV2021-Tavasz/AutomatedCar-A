0b077a4c5486b847d5ba06a3a43910941dae8ab9
Szabolcs Bakos <bakosszaben@gmail.com>
2021-03-04 17:37:27 +0100

    Created PowertrainComponentPacket class

---
+++ src/AutomatedCar/SystemComponents/Packets/PowertrainComponentPacket.cs
@@ -0,0 +1,48 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCar.SystemComponents.Packets
+{
+    class PowertrainComponentPacket : ReactiveObject, IReadOnlyPowertrainComponentPacket
+    {
+        private int x;
+        private int y;
+        private int speed;
+        private int rpm;
+        private double carHeadingAngle;
+
+        public int X
+        {
+            get => this.x;
+            set => this.RaiseAndSetIfChanged(ref this.x, value);
+        }
+
+        public int Y
+        {
+            get => this.y;
+            set => this.RaiseAndSetIfChanged(ref this.y, value);
+        }
+
+        public int Speed
+        {
+            get => this.speed;
+            set => this.RaiseAndSetIfChanged(ref this.speed, value);
+        }
+
+        public int Rpm
+        {
+            get => this.rpm;
+            set => this.RaiseAndSetIfChanged(ref this.rpm, value);
+        }
+
+        public double CarHeadingAngle
+        {
+            get => this.carHeadingAngle;
+            set => this.RaiseAndSetIfChanged(ref this.carHeadingAngle, value);
+        }
+    }
+}

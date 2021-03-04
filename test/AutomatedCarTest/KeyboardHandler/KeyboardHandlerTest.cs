28bcaaf249fe2005780e9c782588162195374d2a
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-04 15:11:45 +0100

    Add empty tests

---
+++ test/AutomatedCarTest/KeyboardHandler/KeyboardHandlerTest.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+namespace AutomatedCarTest.KeyboardHandler
+{
+    public class KeyboardHandlerTest
+    {
+        [Test]
+        public void PressableKeyOnPressEventIsCalledCorrectlyFromKeyboardHandler()
+        {
+
+        }
+
+        [Test]
+        public void HoldableKeyOnHoldEventIsCalledAfterEveryTickFromKeyboardHandlerIfKeyIsPressedAndNotReleased()
+        {
+
+        }
+
+        [Test]
+        public void HoldableKeyOnIdleEventIsCalledAfterEveryTickFromKeyboardHandlerIfKeyIsNotPressed()
+        {
+
+        }
+
+        [Test]
+        public void CanHoldDownMultipleKeys()
+        {
+
+        }
+
+        [Test]
+        public void HoldableKeyDurationIsCorrectAfterSeveralTicksWhenHolding()
+        {
+
+        }
+
+        [Test]
+        public void HoldableKeyDurationIsCorrectAfterSeveralTicksWhenIdle()
+        {
+
+        }
+
+        [Test]
+        public void HoldableKeyDurationResetsAfterStateChange()
+        {
+
+        }
+
+        [Test]
+        public void HoldableKeyStateChangesOnPress()
+        {
+
+        }
+
+        [Test]
+        public void HoldableKeyStateChangesOnRelease()
+        {
+
+        }
+    }
+}

4b0fa4e30f33f89b9cb4df917114fe614b98375a
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-04 17:29:02 +0100

    Add basic KeyboardHandler class

---
+++ src/AutomatedCar/KeyboardHandling/KeyboardHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace AutomatedCar.KeyboardHandling
+{
+    public class KeyboardHandler
+    {
+        public List<PressableKey> PressableKeys { get; } = new List<PressableKey>();
+        public List<HoldableKey> HoldableKeys { get; } = new List<HoldableKey>();
+
+        private double tickInterval;
+
+        public KeyboardHandler(double tickInterval)
+        {
+            this.tickInterval = tickInterval;
+        }
+
+        public void OnKeyDown(Key key)
+        {
+
+        }
+
+        public void OnKeyUp(Key key)
+        {
+
+        }
+
+        public void Tick()
+        {
+
+        }
+    }
+}

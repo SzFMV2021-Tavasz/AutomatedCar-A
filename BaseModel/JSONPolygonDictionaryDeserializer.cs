14ec3cfcf87da614d508333bc72293c928097cf8
Kuklin István Alexander <istvan@codekuklin.com>
2021-03-04 18:10:32 +0100

    Fix: a WorldObject may contain more than one collision polygon

--- BaseModel/JSONPolygonDictionaryDeserializer.cs
+++ BaseModel/JSONPolygonDictionaryDeserializer.cs
@@ -19,14 +19,15 @@ namespace BaseModel
         /// If a typename cannot be found, either in the supplied enum mapping in the polygons file,
         /// then it is going to be interpreted as the object not being able to collide with others.
         /// </summary>
-        public Dictionary<WorldObject.Type, Polygon> Load(string polygonsPath)
+        public Dictionary<WorldObject.Type, List<Polygon>> Load(string polygonsPath)
         {
             Dictionary<WorldObject.Type, Polygon> polygonDictionary = new Dictionary<WorldObject.Type, Polygon>();
             Dictionary<string, Polygon> helper = new Dictionary<string, Polygon>();
 
             string jsonFile = File.ReadAllText(polygonsPath);
             
-            return polygonDictionary = JsonConvert.DeserializeObject<Dictionary<WorldObject.Type, Polygon>>(jsonFile, new JsonHelper.PoligonDictionaryConverter()); ;
+            return null;
+            //return polygonDictionary = JsonConvert.DeserializeObject<Dictionary<WorldObject.Type, Polygon>>(jsonFile, new JsonHelper.PoligonDictionaryConverter()); ;
         }
     }
 }

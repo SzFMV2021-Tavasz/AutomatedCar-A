28902f0164e8275275ac598e2cf8a57ea6bf4040
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-04 17:24:12 +0100

    Add PressableKey class

---
+++ src/AutomatedCar/KeyboardHandling/PressableKey.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace AutomatedCar.KeyboardHandling
+{
+    public class PressableKey
+    {
+        public Key Key { get; }
+
+        public Action OnPress { get; }
+
+        public PressableKey(Key key, Action onPress)
+        {
+            this.Key = key;
+            this.OnPress = onPress;
+        }
+    }
+}

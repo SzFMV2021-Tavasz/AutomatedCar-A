bb5e400b6af0a5dc77c2a83f93998d3922ae0e7c
Szabolcs Bakos <bakosszaben@gmail.com>
2021-03-04 20:47:04 +0100

    Created IVirtualFunctionBus interface - It gives better testability

---
+++ src/AutomatedCar/SystemComponents/IVirtualFunctionBus.cs
@@ -0,0 +1,16 @@
+﻿using AutomatedCar.SystemComponents.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCar.SystemComponents
+{
+    public interface IVirtualFunctionBus
+    {
+        void RegisterComponent(SystemComponent component);
+        IReadOnlyDummyPacket DummyPacket { get; set; }
+        IReadOnlyPowertrainComponentPacket PowertrainPacket { get; set; }
+    }
+}

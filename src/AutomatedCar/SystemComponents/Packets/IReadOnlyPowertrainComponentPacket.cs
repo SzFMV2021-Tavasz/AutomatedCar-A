f3f19401f28f13796134e370d30ef5bda67845f4
Szabolcs Bakos <bakosszaben@gmail.com>
2021-03-04 17:36:19 +0100

    Created IReadOnlyPowertrainComponentPacket interface - Contains all the data/properties that the dashboard needs

---
+++ src/AutomatedCar/SystemComponents/Packets/IReadOnlyPowertrainComponentPacket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCar.SystemComponents.Packets
+{
+    public interface IReadOnlyPowertrainComponentPacket
+    {
+        int X { get; }
+        int Y { get; }
+        int Speed { get; }
+        int Rpm { get; }
+        double CarHeadingAngle { get; }
+    }
+}

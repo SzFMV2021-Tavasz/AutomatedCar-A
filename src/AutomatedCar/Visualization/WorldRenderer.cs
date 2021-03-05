359731b5dbdb59d073b8fdd8bc689aec8bdb5454
Adam Papp <vissiadam@gmail.com>
2021-03-05 18:41:28 +0100

    #38: fixed up issues with previous commit, bound world to the new WorldRenderer element

---
+++ src/AutomatedCar/Visualization/WorldRenderer.cs
@@ -0,0 +1,48 @@
+﻿using AutomatedCar.Models;
+using AutomatedCar.ViewModels;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace AutomatedCar.Visualization
+{
+    public class WorldRenderer : FrameworkElement
+    {
+        public BitmapImage CarSprite { get; set; }
+
+        public World World { get; set; }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            if (World == null) 
+                return;
+
+            foreach (var worldObject in World.WorldObjects)
+            {
+                //worldObject.Render(drawingContext);
+            }
+
+            var car = World.ControlledCar;
+            drawingContext.DrawImage(CarSprite, new Rect(car.X, car.Y, car.Width, car.Height));
+
+            base.OnRender(drawingContext);
+        }
+
+        public WorldRenderer()
+        {
+            Loaded += WorldRenderer_Loaded;
+        }
+
+        private void WorldRenderer_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.World = World.Instance;
+
+            //CarSprite = new BitmapImage();
+            //CarSprite.BeginInit();
+            //CarSprite.UriSource = new System.Uri($"{Directory.GetCurrentDirectory()}/Assets/WorldObjects/{World.ControlledCar.Filename}", System.UriKind.Absolute);
+            //CarSprite.EndInit();
+        }
+    }
+}

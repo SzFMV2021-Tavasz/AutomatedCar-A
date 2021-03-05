eaa62f2a6393912e60b26a1b0edf32517a47eb49
Adam Papp <vissiadam@gmail.com>
2021-03-05 17:24:50 +0100

    create WorldRenderer class, plug it into the View

---
+++ src/AutomatedCar/Rendering/WorldRenderer.cs
@@ -0,0 +1,28 @@
+﻿using AutomatedCar.Models;
+using AutomatedCar.ViewModels;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace AutomatedCar.Rendering
+{
+    internal class WorldRenderer : FrameworkElement
+    {
+        public CourseDisplayViewModel ViewModel { get; set; }
+
+        public ImageSource CarImage { get; set; }
+
+        public ObservableCollection<RenderableWorldObject> renderableWorldObjects => ViewModel.World.WorldObjects;
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            foreach (var worldObject in renderableWorldObjects)
+            {
+                //worldObject.Render(drawingContext);
+            }
+
+            base.OnRender(drawingContext);
+        }
+    }
+}

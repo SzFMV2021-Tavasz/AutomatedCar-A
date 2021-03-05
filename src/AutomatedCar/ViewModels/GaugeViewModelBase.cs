b01acb2c3707acdf2f8c61d375d6a7c50fffd4e6
lkristof <ladanyi.kristof@gmail.com>
2021-03-05 19:29:02 +0100

    Crated base class for gauge type viewmodels.

---
+++ src/AutomatedCar/ViewModels/GaugeViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using ReactiveUI;
+
+namespace AutomatedCar.ViewModels
+{
+    public class GaugeViewModelBase : ViewModelBase
+    {
+        private double _value;
+        private string _caption;
+
+        public GaugeViewModelBase()
+        {
+
+        }
+
+        public double Value
+        {
+            get => this._value;
+            set => this.RaiseAndSetIfChanged(ref this._value, value);
+        }
+
+        public string Caption
+        {
+            get => this._caption;
+            set => this.RaiseAndSetIfChanged(ref this._caption, value);
+        }
+    }
+}

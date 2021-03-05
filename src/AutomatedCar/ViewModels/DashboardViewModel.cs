44ea82cc7f5e498997adc6d9953059d6231a891d
lkristof <ladanyi.kristof@gmail.com>
2021-03-05 19:34:39 +0100

    Fixed datacontext for Dashboard usercontrol. Added properties to dashboard viewmodel for binding.

--- src/AutomatedCar/ViewModels/DashboardViewModel.cs
+++ src/AutomatedCar/ViewModels/DashboardViewModel.cs
@@ -6,6 +6,8 @@ namespace AutomatedCar.ViewModels
     public class DashboardViewModel : ViewModelBase
     {
         private AutomatedCar controlledCar;
+        private RpmGaugeViewModel _rpmGaugeViewModel;
+        private SpeedGaugeViewModel _speedGaugeViewModel;
 
         public DashboardViewModel(Models.AutomatedCar controlledCar)
         {
@@ -16,6 +18,18 @@ namespace AutomatedCar.ViewModels
         {
             get => this.controlledCar;
             private set => this.RaiseAndSetIfChanged(ref this.controlledCar, value);
+        }
+
+        public RpmGaugeViewModel RpmGaugeViewModel
+        {
+            get => this._rpmGaugeViewModel;
+            set => this.RaiseAndSetIfChanged(ref this._rpmGaugeViewModel, value);
+        }
+
+        public SpeedGaugeViewModel SpeedGaugeViewModel
+        {
+            get => this._speedGaugeViewModel;
+            set => this.RaiseAndSetIfChanged(ref this._speedGaugeViewModel, value);
         }
     }
 }

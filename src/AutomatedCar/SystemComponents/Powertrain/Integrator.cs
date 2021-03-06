3316f06b1f488f6950276271c565b5e6dc13cb76
Daniel Meszaros <easimer@gmail.com>
2021-03-06 18:47:18 +0100

    Add the semi-implicit integrator for car phys
    - Implement the semi-implicit Euler integrator for car physics
      - AutomatedCar:
        - Add record VehicleTransform
        - Add enum WheelKind
        - Add interface IIntegrator
        - Add class Integrator
      - AutomatedCarTest:
        - Add Integrator tests

---
+++ src/AutomatedCar/SystemComponents/Powertrain/Integrator.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace AutomatedCar.SystemComponents.Powertrain
+{
+    public class Integrator : IIntegrator
+    {
+        private readonly IVehicleConstants vehicleConstants;
+        private readonly Dictionary<WheelKind, ParticleIntegrator> particles;
+        private float? deltaTime = null;
+        private VehicleTransform? currentTransform = null;
+
+        public VehicleTransform NextVehicleTransform { get => CalculateNextVehicleTransform(); }
+
+        public Integrator(IVehicleConstants vehicleConstants)
+        {
+            this.vehicleConstants = vehicleConstants;
+
+            particles = new Dictionary<WheelKind, ParticleIntegrator>();
+        }
+
+        public void Reset(VehicleTransform vehicleTransform, float deltaTime)
+        {
+            var wheelKinds = Enum.GetValues<WheelKind>();
+            var perParticleMass = vehicleConstants.CurbWeight / wheelKinds.Length;
+
+            foreach (var wheelKind in wheelKinds)
+            {
+                var position = CalculatePositionOfWheel(wheelKind, vehicleTransform.Position, vehicleTransform.AngularDisplacement);
+                var velocity = vehicleTransform.Velocity;
+                particles[wheelKind] = new ParticleIntegrator(position, velocity, perParticleMass, deltaTime);
+            }
+
+            this.currentTransform = vehicleTransform;
+            this.deltaTime = deltaTime;
+        }
+
+        public void AccumulateForce(WheelKind wheel, Vector2 force)
+        {
+            particles[wheel].AccumulateForce(force);
+        }
+
+        private Vector2 CalculatePositionOfWheel(WheelKind wheel, Vector2 carPosition, float carHeading)
+        {
+            float multiplier;
+            switch (wheel)
+            {
+                case WheelKind.Front:
+                    multiplier = +1;
+                    break;
+                case WheelKind.Back:
+                    multiplier = -1;
+                    break;
+                default:
+                    throw new ArgumentException($"{nameof(wheel)} is out-of-range or wasn't handled.");
+            }
+
+            var distanceFromCenterOfMass = vehicleConstants.WheelBase / 2;
+            var direction = GetDirectionVector(carHeading);
+            var particlePosition = carPosition + multiplier * distanceFromCenterOfMass * direction;
+
+            return particlePosition;
+        }
+
+        private Vector2 GetDirectionVector(float carHeading)
+        {
+            var x = (float)Math.Cos(carHeading);
+            var y = (float)Math.Sin(carHeading);
+
+            return new Vector2(x, y);
+        }
+
+        private VehicleTransform CalculateNextVehicleTransform()
+        {
+            var distanceFromCenterOfMass = vehicleConstants.WheelBase / 2;
+
+            var (positionSum, velocitySum, massSum) = particles.Values.Aggregate(
+                (PositionSum: Vector2.Zero, VelocitySum: Vector2.Zero, MassSum: 0f),
+                (acc, P) =>
+                {
+                    var (x, v) = P.NextState;
+                    var m = P.Mass;
+                    return (acc.PositionSum + m * x, acc.VelocitySum + m * v, acc.MassSum + m);
+                }
+            );
+
+            var centerOfMass = positionSum / massSum;
+
+            var angularAccelerationSum = particles.Values.Aggregate(
+                0f,
+                (acc, P) =>
+                {
+                    var (position, velocity) = P.NextState;
+                    var m = P.Mass;
+                    var F = P.AccumulatedForce;
+
+                    // No force acting on this particle
+                    if (F.Length() < float.Epsilon)
+                    {
+                        return 0f;
+                    }
+
+                    var relativePosition = centerOfMass - position;
+                    var torque = CalculateTorqueProducedByForceAtPosition(relativePosition, F);
+
+                    var inertia = m * (distanceFromCenterOfMass * distanceFromCenterOfMass);
+
+                    var angularAcceleration = torque / inertia;
+                    return angularAcceleration;
+                }
+            );
+
+            var steerability = CalculateSteerability(currentTransform.Velocity);
+
+            var nextAngularVelocity = currentTransform.AngularVelocity + steerability * deltaTime.Value * angularAccelerationSum;
+            var nextAngularDisplacement = currentTransform.AngularDisplacement + deltaTime.Value * nextAngularVelocity;
+
+            var nextPosition = centerOfMass;
+            var nextVelocity = velocitySum / massSum;
+
+            return new VehicleTransform(nextPosition, nextAngularDisplacement, nextVelocity, nextAngularVelocity);
+        }
+
+        private float CalculateTorqueProducedByForceAtPosition(Vector2 relativePosition, Vector2 force)
+        {
+            // Magnitude of torque produced by the force at the particle's position is
+            //
+            // torque = [magnitude of relativePosition] * [magnitude of force] * sin(theta)
+            //
+            // where theta is the angle between the two vectors.
+            // sin(theta) can be calculated by
+            //
+            // sin(theta) = sqrt(1 - cos**2(theta))
+            //
+            // and cos(theta) is simply the dot product of the two vectors
+            // after normalization.
+
+            var relativePositionMagnitude = relativePosition.Length();
+            var relativePositionNormalized = relativePosition / relativePositionMagnitude;
+            var forceMagnitude = force.Length();
+            var forceNormalized = force / forceMagnitude;
+
+            var cosineOfAngle = Vector2.Dot(relativePositionNormalized, forceNormalized);
+            var sineOfAngle = Math.Sqrt(1 - cosineOfAngle * cosineOfAngle);
+
+            var torque = relativePositionMagnitude * forceMagnitude * sineOfAngle;
+
+            return (float)torque;
+        }
+
+        private float CalculateSteerability(Vector2 vehicleVelocity)
+        {
+            // Steerability is a function of speed:
+            // s = 1 - (v / v_max) ** 2
+            // Initially it's close to a 100%, but starts to fall off at high
+            // velocities (non-linearly).
+            var maxVelocity = 128;
+            return (float)(1.0 - Math.Pow(vehicleVelocity.Length() / maxVelocity, 2));
+        }
+    }
+}
---
+++ test/AutomatedCarTest/SystemComponents/Powertrain/IntegratorTests.cs
@@ -0,0 +1,88 @@
+﻿using AutomatedCar.SystemComponents.Powertrain;
+using Moq;
+using System.Numerics;
+using Xunit;
+
+namespace AutomatedCarTest.SystemComponents.Powertrain
+{
+    public class IntegratorTests
+    {
+        private readonly Mock<IVehicleConstants> constants = new Mock<IVehicleConstants>();
+
+        public IntegratorTests()
+        {
+            constants.Setup(C => C.CurbWeight).Returns(10);
+            constants.Setup(C => C.WheelBase).Returns(2);
+        }
+
+        [Fact]
+        public void NextVehicleTransformPropertyReturnsInitialTransform()
+        {
+            var initialTransform = new VehicleTransform(Vector2.Zero, 0, Vector2.Zero, 0);
+            var integrator = new Integrator(constants.Object);
+
+            integrator.Reset(initialTransform, 0f);
+
+            var nextTransform = integrator.NextVehicleTransform;
+
+            Assert.Equal(initialTransform, nextTransform);
+        }
+
+        [Fact]
+        public void ForceAppliedIncreasesVelocity()
+        {
+            var initialTransform = new VehicleTransform(Vector2.Zero, 0, Vector2.Zero, 0);
+            var integrator = new Integrator(constants.Object);
+            var deltaTime = 0.1f;
+
+            integrator.Reset(initialTransform, deltaTime);
+            integrator.AccumulateForce(WheelKind.Front, Vector2.UnitX);
+            integrator.AccumulateForce(WheelKind.Back, Vector2.UnitX);
+            var nextTransform = integrator.NextVehicleTransform;
+
+            Assert.True(nextTransform.Velocity.Length() > initialTransform.Velocity.Length());
+        }
+
+        [Fact]
+        public void InverseForceAppliedDecreasesVelocity()
+        {
+            var initialTransform = new VehicleTransform(Vector2.Zero, 0, Vector2.UnitX, 0);
+            var integrator = new Integrator(constants.Object);
+            var deltaTime = 0.1f;
+
+            integrator.Reset(initialTransform, deltaTime);
+            integrator.AccumulateForce(WheelKind.Front, -initialTransform.Velocity);
+            integrator.AccumulateForce(WheelKind.Back, -initialTransform.Velocity);
+            var nextTransform = integrator.NextVehicleTransform;
+
+            Assert.True(nextTransform.Velocity.Length() < initialTransform.Velocity.Length());
+        }
+
+        [Fact]
+        public void TorqueTest()
+        {
+            // Apply two forces on each end of the vehicle, such that the front
+            // is pushed in the Y+ direction and the back is pushed in the Y-
+            // direction.
+            // This should make the body (in absence of other forces) rotate
+            // around its center of mass.
+            //
+            //      ↓
+            //     +--------+
+            //     |        |
+            //     +--------+
+            //            ↑
+
+            var initialTransform = new VehicleTransform(Vector2.Zero, 0, Vector2.Zero, 0);
+            var integrator = new Integrator(constants.Object);
+            var deltaTime = 0.1f;
+
+            integrator.Reset(initialTransform, deltaTime);
+            integrator.AccumulateForce(WheelKind.Front, +10 * Vector2.UnitY);
+            integrator.AccumulateForce(WheelKind.Back, -10 * Vector2.UnitY);
+            var nextTransform = integrator.NextVehicleTransform;
+
+            Assert.True(nextTransform.AngularDisplacement > initialTransform.AngularDisplacement);
+        }
+    }
+}

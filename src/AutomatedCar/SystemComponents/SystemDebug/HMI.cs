f3d1612464310b21d03ec15a706ddb7e21943896
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-06 13:33:37 +0100

    Make tests pass

--- src/AutomatedCar/SystemComponents/SystemDebug/HMI.cs
+++ src/AutomatedCar/SystemComponents/SystemDebug/HMI.cs
@@ -10,7 +10,24 @@ namespace AutomatedCar.SystemComponents.SystemDebug
 
         public void OnDebugAction(int debugArgId)
         {
-
+            switch (debugArgId)
+            {
+                case 1:
+                    args.DebugRadar = !args.DebugRadar;
+                    break;
+                case 2:
+                    args.DebugSonic = !args.DebugSonic;
+                    break;
+                case 3:
+                    args.DebugVideo = !args.DebugVideo;
+                    break;
+                case 4:
+                    args.DebugPolys = !args.DebugPolys;
+                    break;
+                default:
+                    break;
+            }
+            DebugActionEventHandler?.Invoke(this, args);
         }
     }
 }

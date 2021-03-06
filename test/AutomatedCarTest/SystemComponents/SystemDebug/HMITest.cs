bb8c98e2430a799d3b680907444305017799565d
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-06 13:34:18 +0100

    Remove unused usings from test class

--- test/AutomatedCarTest/SystemComponents/SystemDebug/HMITest.cs
+++ test/AutomatedCarTest/SystemComponents/SystemDebug/HMITest.cs
@@ -1,10 +1,5 @@
 ﻿using AutomatedCar.SystemComponents.SystemDebug;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Test.SystemComponents.SystemDebug
 {

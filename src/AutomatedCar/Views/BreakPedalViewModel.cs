bd9af3537a387c4cd82526350c5ccfa9e6915e50
lkristof <ladanyi.kristof@gmail.com>
2021-03-06 18:14:34 +0100

    Added ViewModel for break pedal.

---
+++ src/AutomatedCar/Views/BreakPedalViewModel.cs
@@ -0,0 +1,27 @@
+﻿using AutomatedCar.ViewModels;
+using ReactiveUI;
+
+namespace AutomatedCar.Views
+{
+    public class BreakPedalViewModel : ViewModelBase
+    {
+        private int _value;
+        private string _caption;
+
+        public BreakPedalViewModel()
+        {
+        }
+
+        public int Value
+        {
+            get => this._value;
+            set => this.RaiseAndSetIfChanged(ref this._value, value);
+        }
+
+        public string Caption
+        {
+            get => this._caption;
+            set => this.RaiseAndSetIfChanged(ref this._caption, value);
+        }
+    }
+}

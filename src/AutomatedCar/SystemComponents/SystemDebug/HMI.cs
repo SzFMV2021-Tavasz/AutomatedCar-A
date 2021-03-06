da99e8cbf6fdb2f65489343f1ae6c9a3e9a39eb2
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-06 13:32:37 +0100

    Create HMI.cs

---
+++ src/AutomatedCar/SystemComponents/SystemDebug/HMI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AutomatedCar.SystemComponents.SystemDebug
+{
+    public class HMI
+    {
+        private DebugActionArgs args = new DebugActionArgs();
+
+        public static event EventHandler<DebugActionArgs> DebugActionEventHandler;
+
+        public void OnDebugAction(int debugArgId)
+        {
+
+        }
+    }
+}

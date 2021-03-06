05c71a4fd4bd05cffab1ba49e766ea188c80ae1c
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-06 12:39:42 +0100

    Create DebugActionArgs.cs

---
+++ src/AutomatedCar/SystemComponents/SystemDebug/DebugActionArgs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AutomatedCar.SystemComponents.Debug
+{
+    class DebugActionArgs : EventArgs
+    {
+        public bool DebugRadar { get; set; }
+
+        public bool DebugSonic { get; set; }
+
+        public bool DebugVideo { get; set; }
+
+        public bool DebugPolys { get; set; }
+    }
+}

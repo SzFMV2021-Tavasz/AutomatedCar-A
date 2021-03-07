8f6774a8a9f6fce4c0a4bbfef79da0718fc609cf
lkristof <ladanyi.kristof@gmail.com>
2021-03-07 14:18:39 +0100

    Added ViewModel for ACC related information. Wrapped acc distance options in a seperate model.

---
+++ src/AutomatedCar/Models/ACCDesiredDistanceOptions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AutomatedCar.Models
+{
+    public class ACCDesiredDistanceOptions
+    {
+        private IReadOnlyList<double> DesiredDistanceOptionsInSeconds = new double[] { 0.8, 1.0, 1.2, 1.4 };
+        private int _currentIndex;
+
+        public ACCDesiredDistanceOptions(IEnumerable<double> desiredDistanceOptions = null)
+        {
+            if (desiredDistanceOptions is not null)
+            {
+                this.DesiredDistanceOptionsInSeconds = desiredDistanceOptions.ToArray();
+            }
+
+            this._currentIndex = 0;
+        }
+
+        public double GetDefault() 
+        {
+            return 0;
+        }
+
+        public double GetNext()
+        {
+            return 0;
+        }
+
+        public double GetPrevious()
+        {
+            return 0;
+        }
+    }
+}
---
+++ src/AutomatedCar/ViewModels/ACCOptionsViewModel.cs
@@ -0,0 +1,70 @@
+﻿using AutomatedCar.Models;
+using ReactiveUI;
+
+namespace AutomatedCar.ViewModels
+{
+    public class ACCOptionsViewModel : ViewModelBase
+    {
+        private const int MaxDesiredSpeed = 160;
+        private const int MinDesiredSpeed = 30;
+        private const int DesiredSpeedStep = 10;
+
+        private readonly ACCDesiredDistanceOptions _accDesiredDistanceOptions;
+        private double _desiredDistanceInSeconds;
+        private int _desiredSpeed;
+        private bool _isTurnedOn;
+
+        public ACCOptionsViewModel()
+        {
+            this._accDesiredDistanceOptions = new ACCDesiredDistanceOptions();
+        }
+
+        public double DesiredDistanceInSeconds
+        {
+            get => this._desiredDistanceInSeconds;
+            set => this.RaiseAndSetIfChanged(ref this._desiredDistanceInSeconds, value);
+        }
+
+        public int DesiredSpeed
+        {
+            get => this._desiredSpeed;
+            set => this.RaiseAndSetIfChanged(ref this._desiredSpeed, value);
+        }
+
+        public bool IsTurnedOn
+        {
+            get => this._isTurnedOn;
+            set => this.RaiseAndSetIfChanged(ref this._isTurnedOn, value);
+        }
+
+        public void IncreaseDesiredSpeed()
+        {
+            if (this.DesiredSpeed + DesiredSpeedStep > MaxDesiredSpeed)
+            {
+                return;
+            }
+
+            this.DesiredSpeed += DesiredSpeedStep;
+        }
+
+        public void DecreaseDesiredSpeed() 
+        {
+            if (this.DesiredSpeed - DesiredSpeedStep < MinDesiredSpeed)
+            {
+                return;
+            }
+
+            this.DesiredSpeed -= DesiredSpeedStep;
+        }
+
+        public void SetToNextDesiredDistance()
+        {
+
+        }
+
+        public void SetToPreviousDesiredDistance()
+        {
+
+        }
+    }
+}

c1cbab8ddc73f1bdabc42c81c681c70a576796b1
kissamon <kissamon@stud.uni-obuda.hu>
2021-03-07 23:21:13 +0100

    Create PriorityChecker class.

---
+++ src/AutomatedCar/SystemComponents/Powertrain/PriorityChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCar.SystemComponents.Powertrain
+{
+    class PriorityChecker : IPriorityChecker
+    {
+        public IVirtualFunctionBus virtualFunctionBus { get; set; }
+
+        public IVehicleForces vehicleForces { get; set; }
+
+        public void AccelerationPriorityCheck()
+        {
+        }
+
+        public void SteeringPriorityCheck()
+        {
+        }
+    }
+}

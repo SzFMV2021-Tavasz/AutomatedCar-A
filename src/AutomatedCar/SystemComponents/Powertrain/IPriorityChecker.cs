c4a389fa510d0002b366e6e75093b766006032d1
kissamon <kissamon@stud.uni-obuda.hu>
2021-03-07 23:20:53 +0100

    Create IPriorityChecker interface.

---
+++ src/AutomatedCar/SystemComponents/Powertrain/IPriorityChecker.cs
@@ -0,0 +1,13 @@
+﻿namespace AutomatedCar.SystemComponents.Powertrain
+{
+    public interface IPriorityChecker
+    {
+        IVirtualFunctionBus virtualFunctionBus { get; set; }
+
+        IVehicleForces vehicleForces { get; set; }
+
+        void AccelerationPriorityCheck();
+
+        void SteeringPriorityCheck();
+    }
+}

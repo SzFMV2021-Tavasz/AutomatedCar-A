b64b127f4156cbfae99f20d4d7993fb12e6d9dc4
lkristof <ladanyi.kristof@gmail.com>
2021-03-07 17:01:03 +0100

    Added ViewModel for lane keeping and parking pilot functions, added tests for ViewModel.

---
+++ src/AutomatedCar/ViewModels/LaneKeepingAndParkingPilotViewModel.cs
@@ -0,0 +1,43 @@
+﻿using ReactiveUI;
+
+namespace AutomatedCar.ViewModels
+{
+    public class LaneKeepingAndParkingPilotViewModel : ViewModelBase
+    {
+        private bool _isLaneKeepingTurnedOn;
+        private bool _isParkingPilotTurnedOn;
+        private bool _isLaneKeepingWarningTurnedOn;
+
+        public LaneKeepingAndParkingPilotViewModel()
+        {
+
+        }
+
+        public bool IsLaneKeepingTurnedOn
+        {
+            get => this._isLaneKeepingTurnedOn;
+            private set => this.RaiseAndSetIfChanged(ref this._isLaneKeepingTurnedOn, value);
+        }
+
+        public bool IsParkingPilotTurnedOn
+        {
+            get => this._isParkingPilotTurnedOn;
+            private set => this.RaiseAndSetIfChanged(ref this._isParkingPilotTurnedOn, value);
+        }
+
+        public bool IsLaneKeepingWarningTurnedOn
+        {
+            get => this._isLaneKeepingWarningTurnedOn;
+            private set => this.RaiseAndSetIfChanged(ref this._isLaneKeepingWarningTurnedOn, value);
+        }
+
+        public void ToggleLaneKeeping() => this.IsLaneKeepingTurnedOn = !this.IsLaneKeepingTurnedOn;
+
+        public void ToggleParkingPilot() => this.IsParkingPilotTurnedOn = !this.IsParkingPilotTurnedOn;
+
+        public void DisplayLaneKeepingWarning() => this.IsLaneKeepingWarningTurnedOn = true;
+
+        public void TurnOffLaneKeepingWarning() => this.IsLaneKeepingWarningTurnedOn = false;
+
+    }
+}

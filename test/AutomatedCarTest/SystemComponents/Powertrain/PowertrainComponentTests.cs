0f5b4d00edefc46e8b6fe525e5513c6126b62f59
kissamon <kissamon@stud.uni-obuda.hu>
2021-03-07 23:23:20 +0100

    Added PriorityChecker tests.

--- test/AutomatedCarTest/SystemComponents/Powertrain/PowertrainComponentTests.cs
+++ test/AutomatedCarTest/SystemComponents/Powertrain/PowertrainComponentTests.cs
@@ -12,22 +12,33 @@ namespace Test.SystemComponents.Powertrain
 {
     public class PowertrainComponentTests
     {
+        Mock<IVirtualFunctionBus> mockBus = new Mock<IVirtualFunctionBus>();
+        Mock<IPriorityChecker> mockedPriorityChecker = new Mock<IPriorityChecker>();
+
+        IVirtualFunctionBus virtualFunctionBus = new VirtualFunctionBus();
+
         [Fact]
         public  void PowertrainAutomaticallyGetsRegisteredToVFB()
         {
-            Mock<IVirtualFunctionBus> mockBus = new Mock<IVirtualFunctionBus>();
-            PowertrainComponent ptr = new PowertrainComponent(mockBus.Object);
-
+            PowertrainComponent ptr = new PowertrainComponent(mockBus.Object, mockedPriorityChecker.Object, null);
             mockBus.Verify(m => m.RegisterComponent(ptr), Times.Once);
         }
 
         [Fact]
         public void VFBGetsPowertrainPacketAfterCreatingPowertrainComponent()
         {
-            VirtualFunctionBus virtualFunctionBus = new VirtualFunctionBus();
-            PowertrainComponent powertrain = new PowertrainComponent(virtualFunctionBus);
-
+            PowertrainComponent powertrain = new PowertrainComponent(virtualFunctionBus, mockedPriorityChecker.Object, null);
             Assert.True(virtualFunctionBus.PowertrainPacket != null);
         }
+
+        [Fact]
+        public void PriorityCheckerCheckingMethodsInvokedInPowertrainComponentProcessMethod()
+        {
+            PowertrainComponent powertrainComponent = new PowertrainComponent(virtualFunctionBus, mockedPriorityChecker.Object, null);
+            powertrainComponent.Process();
+            mockedPriorityChecker.Verify(c => c.AccelerationPriorityCheck(), Times.Once);
+            mockedPriorityChecker.Verify(c => c.SteeringPriorityCheck(), Times.Once);
+        }
+
     }
 }

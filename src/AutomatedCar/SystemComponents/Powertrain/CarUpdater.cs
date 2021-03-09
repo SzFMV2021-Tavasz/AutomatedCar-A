2cae2f6280d0abf9773b57dcfbdca07a5627b5a5
Szabolcs Bakos <bakosszaben@gmail.com>
2021-03-09 14:44:33 +0100

    Created CarUpdater class with basic initialization.

---
+++ src/AutomatedCar/SystemComponents/Powertrain/CarUpdater.cs
@@ -0,0 +1,65 @@
+﻿using AutomatedCar.Models;
+using AutomatedCar.SystemComponents.Packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCar.SystemComponents.Powertrain
+{
+    public class CarUpdater : ICarUpdater
+    {
+        public IVirtualFunctionBus VirtualFunctionBus { get; }
+        public IVehicleForces VehicleForces { get; }
+        public IIntegrator Integrator { get;}
+
+        private PowertrainComponentPacket powertrainComponentPacket;
+        private IPriorityChecker priorityChecker;
+        private VehicleTransform currentTransform;
+        private Vector2 carPos;
+        private float currentSteering;
+        private Vector2 currentWheelDirection;
+        private float currentDirection;
+        private float deltaTime = 1 / 30;
+        public CarUpdater(IVirtualFunctionBus virtualFunctionBus, IVehicleForces vehicleForces, IIntegrator integrator, PowertrainComponentPacket powertrainPacket)
+        {
+            this.VirtualFunctionBus = virtualFunctionBus;
+            this.VehicleForces = vehicleForces;
+            this.Integrator = integrator;
+            this.powertrainComponentPacket = powertrainPacket;
+
+            currentSteering = World.Instance.ControlledCar.CurrentSteering;
+            currentWheelDirection = new Vector2((float)Math.Cos(currentDirection), (float)Math.Sin(currentDirection));
+            currentDirection = World.Instance.ControlledCar.CarHeading;
+            priorityChecker = new PriorityChecker();
+            priorityChecker.virtualFunctionBus = this.VirtualFunctionBus;
+            CreateCurrentTransform();
+        }
+
+        private void CreateCurrentTransform()
+        {
+            this.carPos = new Vector2(World.Instance.ControlledCar.X, World.Instance.ControlledCar.Y);
+            currentTransform = new VehicleTransform(carPos, World.Instance.ControlledCar.CarHeading, World.Instance.ControlledCar.Velocity, World.Instance.ControlledCar.AngularVelocity);
+        }
+        public void UpdateWorldObject()
+        {
+           
+            
+        }
+        public void UpdatePacket()
+        {
+            
+        }
+        public void Calculate()
+        {
+           
+
+        }
+        public void SetCurrentTransform()
+        {
+            
+        }
+    }
+}

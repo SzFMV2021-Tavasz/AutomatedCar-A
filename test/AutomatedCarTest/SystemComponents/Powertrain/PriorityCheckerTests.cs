26d369734f4f78d3bf3bcc30076bdf75d092ce67
kissamon <kissamon@stud.uni-obuda.hu>
2021-03-09 14:19:26 +0100

    Creted tests for SteeringWheelPriorityChecking.

---
+++ test/AutomatedCarTest/SystemComponents/Powertrain/PriorityCheckerTests.cs
@@ -0,0 +1,43 @@
+﻿using AutomatedCar.SystemComponents;
+using AutomatedCar.SystemComponents.Packets;
+using AutomatedCar.SystemComponents.Powertrain;
+using Moq;
+using Xunit;
+
+namespace Test.SystemComponents.Powertrain
+{
+    public class PriorityCheckerTests
+    {
+        IPriorityChecker PriorityChecker = new PriorityChecker();
+        IVirtualFunctionBus virtualFunctionBus = new VirtualFunctionBus();
+
+        [Fact]
+        public void SteeringWheelPriorityCheckingHMIPacket()
+        {
+            Mock<IReadOnlyHMIPacket> mockedHMI = new Mock<IReadOnlyHMIPacket>();
+            virtualFunctionBus.HMIPacket = mockedHMI.Object;
+            PriorityChecker.virtualFunctionBus = virtualFunctionBus;
+
+            Assert.True(PriorityChecker.SteeringPriorityCheck() == PacketEnum.HMI);
+        }
+
+        [Fact]
+        public void SteeringWheelPriorityCheckingLKAPacket()
+        {
+            Mock<IReadOnlyLKAPacket> mockedLKA = new Mock<IReadOnlyLKAPacket>();
+            virtualFunctionBus.LKAPacket = mockedLKA.Object;
+            PriorityChecker.virtualFunctionBus = virtualFunctionBus;
+
+            Assert.True(PriorityChecker.SteeringPriorityCheck() == PacketEnum.LKA);
+        }
+
+        [Fact]
+        public void SteeringWheelPriorityCheckingPPPacket()
+        {
+            Mock<IReadOnlyPPPacket> mockedPP = new Mock<IReadOnlyPPPacket>();
+            virtualFunctionBus.PPPacket = mockedPP.Object;
+
+            Assert.True(PriorityChecker.SteeringPriorityCheck() == PacketEnum.PP);
+        }
+    }
+}

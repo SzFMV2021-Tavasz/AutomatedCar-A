3bf42649bdc1617db58f2028c453bfc5e4b9bef1
Szabolcs Bakos <bakosszaben@gmail.com>
2021-03-09 10:31:15 +0100

    Created ICarUpdater interface.

---
+++ src/AutomatedCar/SystemComponents/Powertrain/ICarUpdater.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCar.SystemComponents.Powertrain
+{
+    public interface ICarUpdater
+    {
+        IVirtualFunctionBus VirtualFunctionBus { get; }
+        IVehicleForces VehicleForces { get; }
+        IIntegrator Integrator { get; }
+        void Calculate();
+        void UpdateWorldObject();
+        void UpdatePacket();
+    }
+}

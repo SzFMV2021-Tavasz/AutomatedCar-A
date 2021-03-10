32d49ef0131c9dd5dfe3f43cc6225209ce21c6a7
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-10 12:42:54 +0100

    Create HMIPacket.cs

---
+++ src/AutomatedCar/SystemComponents/Packets/HMIPacket.cs
@@ -0,0 +1,28 @@
+﻿using AutomatedCar.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCar.SystemComponents.Packets
+{
+    public class HMIPacket : IReadOnlyHMIPacket
+    {
+        public HMIPacket(int gasPedal, int brakePedal, int steeringWheelAngle, Gear gear)
+        {
+            this.GasPedal = gasPedal;
+            this.BrakePedal = brakePedal;
+            this.SteeringWheelAngle = steeringWheelAngle;
+            this.Gear = gear;
+        }
+
+        public int GasPedal { get; }
+
+        public int BrakePedal { get; }
+
+        public double SteeringWheelAngle { get; }
+
+        public Gear Gear { get; }
+    }
+}

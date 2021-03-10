300e58d985d7032e01af96c9be1f4d9bde612593
Szabolcs Bakos <bakosszaben@gmail.com>
2021-03-10 12:27:45 +0100

    Created ITransmission interface.

---
+++ src/AutomatedCar/SystemComponents/Powertrain/ITransmission.cs
@@ -0,0 +1,17 @@
+﻿using AutomatedCar.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCar.SystemComponents.Powertrain
+{
+    public interface ITransmission
+    {
+        Gear Gear { get; set; }
+        int InsideGear { get; set; }
+        void SetInsideGear(int rpm);
+    
+    }
+}

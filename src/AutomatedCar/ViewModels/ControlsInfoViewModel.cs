48eac7ad330bafc9eae9f5fca8be4ce563e7d8b5
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-16 13:54:39 +0100

    Create ControlsInfoViewModel.cs

---
+++ src/AutomatedCar/ViewModels/ControlsInfoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace AutomatedCar.ViewModels
+{
+    public class ControlsInfoViewModel : ViewModelBase
+    {
+        private string controlsInfoKey;
+
+        public ControlsInfoViewModel(string controlsInfoKey)
+        {
+            this.controlsInfoKey = controlsInfoKey;
+        }
+
+        public string Caption => $"Press {controlsInfoKey} to see controls";
+    }
+}

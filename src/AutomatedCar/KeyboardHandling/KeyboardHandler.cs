e7efec6c2108227c6dd74fee1913d5bc0dbe69e6
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-16 13:54:22 +0100

    Move some properties to be below contructor

--- src/AutomatedCar/KeyboardHandling/KeyboardHandler.cs
+++ src/AutomatedCar/KeyboardHandling/KeyboardHandler.cs
@@ -5,15 +5,16 @@ namespace AutomatedCar.KeyboardHandling
 {
     public class KeyboardHandler
     {
-        public List<PressableKey> PressableKeys { get; } = new List<PressableKey>();
-        public List<HoldableKey> HoldableKeys { get; } = new List<HoldableKey>();
-
         private double tickInterval;
 
         public KeyboardHandler(double tickInterval)
         {
             this.tickInterval = tickInterval;
         }
+
+        public List<PressableKey> PressableKeys { get; } = new List<PressableKey>();
+
+        public List<HoldableKey> HoldableKeys { get; } = new List<HoldableKey>();
 
         public void OnKeyDown(Key key)
         {

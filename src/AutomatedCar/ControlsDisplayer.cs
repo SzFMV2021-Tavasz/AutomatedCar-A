90c03aa61f0ccddd34adc059bd31e9f2de05b946
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-16 13:53:48 +0100

    Create ControlsDisplayer.cs

---
+++ src/AutomatedCar/ControlsDisplayer.cs
@@ -0,0 +1,27 @@
+﻿using AutomatedCar.KeyboardHandling;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace AutomatedCar
+{
+    public class ControlsDisplayer
+    {
+        private List<PressableKey> pressableKeys;
+        private List<HoldableKey> holdableKeys;
+
+        public ControlsDisplayer(List<PressableKey> pressableKeys, List<HoldableKey> holdableKeys)
+        {
+            this.pressableKeys = pressableKeys;
+            this.holdableKeys = holdableKeys;
+        }
+
+        public void DisplayControls()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            holdableKeys.ForEach(key => stringBuilder.Append($"{key.Control}: {key.Key}\n"));
+            pressableKeys.ForEach(key => stringBuilder.Append($"{key.Control}: {key.Key}\n"));
+            MessageBox.Show(stringBuilder.ToString());
+        }
+    }
+}

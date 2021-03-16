0ad95271c2c32d47f3c12bbe6542d2a97afec5d5
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-16 14:01:29 +0100

    Create InputKey.cs to serve as base for HoldableKey and PressableKey

---
+++ src/AutomatedCar/KeyboardHandling/InputKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+
+namespace AutomatedCar.KeyboardHandling
+{
+    public abstract class InputKey
+    {
+        public InputKey(Key key, string control)
+        {
+            this.Key = key;
+            this.Control = control;
+        }
+
+        public Key Key { get; }
+
+        public string Control { get; }
+    }
+}

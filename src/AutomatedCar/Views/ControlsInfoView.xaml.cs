fa945763c8d75ae9da6db187342b9b2e1ac1fb38
Dorián Engel <engel.dorian@stud.uni-obuda.hu>
2021-03-16 13:55:15 +0100

    Create ControlsInfoView.xaml + xaml.cs

---
+++ src/AutomatedCar/Views/ControlsInfoView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace AutomatedCar.Views
+{
+    /// <summary>
+    /// Interaction logic for ControlsInfoView.xaml
+    /// </summary>
+    public partial class ControlsInfoView : UserControl
+    {
+        public ControlsInfoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
